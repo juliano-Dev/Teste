74de4d7ce5c94c984e9e981431edb265b5a1ced6
Juliano Gomes <julianocg2013@gmail.com>
2020-08-29 20:25:54 -0300

    Primeiro commit

---
+++ src/main/java/com/testeecommerce/teste/TesteEcommerceSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.testeecommerce.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteEcommerceSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteEcommerceSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testeecommerce/teste/TesteEcommerceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testeecommerce.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteEcommerceSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
